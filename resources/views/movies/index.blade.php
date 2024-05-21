fc86d63a620108f4c225d08d21a5ddd4fe523da2
DavideAlovisio13 <alovisiod@gmail.com>
2024-05-21 16:08:05 +0200

    (app.scss) style elements

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -5,16 +5,16 @@
 @section('content')
 <main id="all-movies">
     <h1>Movies</h1>
-    <div class="container d-flex flex-wrap justify-content-center gap-3">
+    <div class="container d-flex flex-wrap justify-content-center gap-4">
         @foreach ( $movies as $movie )
         <div class="card">
             <img src="{{ $movie->image }}" class="card-img-top" alt="cover">
             <div class="card-body">
                 <h5 class="card-title">{{ $movie->title }}</h5>
-                <p class="card-text">Original Title: {{ $movie->original_title }}</p>
-                <p class="card-text">Language: {{ $movie->language }}</p>
-                <p class="card-text">Date: {{ $movie->date }}</p>
-                <p class="card-text">Vote: {{ $movie->vote }}</p>
+                <p class="card-text"><strong>Original Title:</strong> {{ $movie->original_title }}</p>
+                <p class="card-text"><strong>Language</strong>: {{ $movie->language }}</p>
+                <p class="card-text"><strong>Date:</strong> {{ $movie->date }}</p>
+                <p class="card-text"><strong>Vote:</strong> {{ $movie->vote }}</p>
                 <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
         </div>
