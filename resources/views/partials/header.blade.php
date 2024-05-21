fd153b562052dcea43d5ebcd441baa4d4f0492ce
DavideAlovisio13 <alovisiod@gmail.com>
2024-05-21 17:13:07 +0200

    (header.blade) create nav-bar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,23 @@
 <header>
-    <a class="nav-link  {{Route::currentRouteName() == 'movies.index' ? 'active' : ''}}" href="{{route('movies.index')}}">Movies</a>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Laravel Movies and Books</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link {{Route::currentRouteName() == 'home' ? 'active' : ''}}" aria-current="page" href="{{route('home.index')}}">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link {{Route::currentRouteName() == 'movies.index' ? 'active' : ''}}" href="{{route('movies.index')}}">Movies</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 </header>
